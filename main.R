2ae68c3df2bc9127f2ecb557731f745c3c7ebee1
logstar <y.will.zhang@gmail.com>
2021-08-24 12:38:55 -0400

    Refactor main.R to remove library(plumber)
    Reduce the chance of name conflicts.

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
-library(plumber)
+# Adapted from https://www.rplumber.io/articles/quickstart.html
 
-pr("plumber.R") %>%
-  pr_run(port=80, host="0.0.0.0")
+# Get %>% without loading the whole library
+`%>%` <- magrittr::`%>%`
+
+plumber::pr("plumber.R") %>%
+  plumber::pr_run(port=80, host="0.0.0.0")
