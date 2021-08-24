a83aa832918c530206776821e9057805a6632c9e
logstar <y.will.zhang@gmail.com>
2021-08-24 16:56:06 -0400

    Extract data prep to src/get_tpm_boxplot_data.R

--- main.R
+++ main.R
@@ -1,2 +1,11 @@
+# main.R is OpenPedCan-api entrypoint.
+#
+# This file should be run with the directory that contains this file as working
+# directory.
+#
+# Call sequence:
+#
+# - docker run calls Rscript --vanilla main.R
+
 # Adapted from https://www.rplumber.io/articles/quickstart.html
 plumber::pr_run(plumber::pr("src/plumber.R"), port=80, host="0.0.0.0")
