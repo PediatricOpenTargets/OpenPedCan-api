d32b5e7d7aae53e003c4ba8a0484e54acd4b9bc3
logstar <y.will.zhang@gmail.com>
2021-08-25 02:00:05 -0400

    Add comments on how to manage R scripts in src

--- main.R
+++ main.R
@@ -17,6 +17,18 @@
 #   arrives, but variables in R_GlobalEnv and X can be accessed by plumber
 #   endpoints. Y inherits X (?).
 
+# All R scripts are sourced in .GlobalEnv, by design. Although R scripts can
+# overwrite predefined variables in the .GlobalEnv, it is much more complicated
+# to use R environment/(sys.)frame functionalities to manage environments/frames
+# of the R scripts.
+#
+# To add a new script:
+#
+# - Only keep one variable in the end, if possible, by rm intermediate variables
+#   that are not going to be used by other scripts. This way, global variables
+#   can be tracked straightforwardly.
+# - Use ls() to check what variables are defined by previous scripts, make sure
+#   previously defined variables are not overwritten by the new script.
 source("src/tpm_data_lists.R", chdir = TRUE)
 source("src/get_single_cancer_tpm_boxplot_tbl.R")
 
