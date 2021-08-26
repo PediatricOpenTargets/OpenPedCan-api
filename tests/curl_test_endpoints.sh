a9117cccfe371308d5c51f7e1ca1fbb533a8f4fd
logstar <y.will.zhang@gmail.com>
2021-08-26 17:58:50 -0400

    Set tests/curl_test_endpoints.sh working dir

--- tests/curl_test_endpoints.sh
+++ tests/curl_test_endpoints.sh
@@ -3,6 +3,12 @@ set -e
 set -u
 set -o pipefail
 
+# This script should always run as if it were being called from
+# the directory it lives in.
+#
+# Adapted from https://stackoverflow.com/a/3355423/4638182
+cd "$(dirname "$0")" || exit
+
 # Set API_PORT to 8082 if not set.
 #
 # Adapted from
@@ -17,16 +23,16 @@ fi
 printf "%0.s\n" {1..50}
 
 set -x
-curl -s -v "http://localhost:${API_PORT}/tpm/gene-disease-gtex/json?ensemblId=ENSG00000213420&efoId=EFO_0000621" > tests/results/test-gene-disease-gtex.json
+curl -s -v "http://localhost:${API_PORT}/tpm/gene-disease-gtex/json?ensemblId=ENSG00000213420&efoId=EFO_0000621" > results/test-gene-disease-gtex.json
 
 printf "%0.s\n" {1..6}
 
-curl -s -v "http://localhost:${API_PORT}/tpm/gene-disease-gtex/plot?ensemblId=ENSG00000213420&efoId=EFO_0000621" > tests/plots/test-gene-disease-gtex.png
+curl -s -v "http://localhost:${API_PORT}/tpm/gene-disease-gtex/plot?ensemblId=ENSG00000213420&efoId=EFO_0000621" > plots/test-gene-disease-gtex.png
 
 printf "%0.s\n" {1..6}
 
-curl -s -v "http://localhost:${API_PORT}/tpm/gene-all-cancer/json?ensemblId=ENSG00000213420" > tests/results/test-gene-all-cancer.json
+curl -s -v "http://localhost:${API_PORT}/tpm/gene-all-cancer/json?ensemblId=ENSG00000213420" > results/test-gene-all-cancer.json
 
 printf "%0.s\n" {1..6}
 
-curl -s -v "http://localhost:${API_PORT}/tpm/gene-all-cancer/plot?ensemblId=ENSG00000213420" > tests/plots/test-gene-all-cancer.png
+curl -s -v "http://localhost:${API_PORT}/tpm/gene-all-cancer/plot?ensemblId=ENSG00000213420" > plots/test-gene-all-cancer.png
