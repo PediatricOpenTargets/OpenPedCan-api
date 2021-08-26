e37afb292cee30635ac59734dc632caec8eb6402
logstar <y.will.zhang@gmail.com>
2021-08-26 17:54:40 -0400

    Add tests/run_r_lintr.sh

---
+++ tests/run_r_lintr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+#
+# Adapted from https://stackoverflow.com/a/3355423/4638182
+cd "$(dirname "$0")" || exit
+
+Rscript --vanilla -e "lintr::lint_dir(dir = '..', linters = lintr::with_defaults(object_name_linter = NULL, assignment_linter = NULL, line_length_linter = NULL, spaces_left_parentheses_linter = NULL, commented_code_linter = NULL, object_length_linter = NULL, cyclocomp_linter = lintr::cyclocomp_linter(complexity_limit = 25L)))"
