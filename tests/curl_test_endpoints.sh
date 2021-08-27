e499c303dd8dbc3cfbf5a41af0020f1e2bf588b1
logstar <y.will.zhang@gmail.com>
2021-08-27 00:05:26 -0400

    Convert JSON to TSV for review

--- tests/curl_test_endpoints.sh
+++ tests/curl_test_endpoints.sh
@@ -32,6 +32,8 @@ for ensg_id in "ENSG00000213420" "ENSG00000157764"; do
     output_fn="test-tpm-gene-disease-gtex-${ensg_id}-${efo_id}.json"
     curl -X "${http_request_method}" -s -w "${http_request_method} ${query_url}\nhttp_code: %{http_code}\ncontent_type: %{content_type}\ntime_total: %{time_total} seconds\n\n\n" -o "${output_dir}/${output_fn}" "${query_url}"
     jq '.[0:3]' "${output_dir}/${output_fn}"
+    # convert json to tsv
+    Rscript --vanilla -e "readr::write_tsv(jsonlite::fromJSON('${output_dir}/${output_fn}'), 'results/${output_fn%.json}.tsv')"
 
     query_url="${base_url}/tpm/gene-disease-gtex/plot?ensemblId=${ensg_id}&efoId=${efo_id}"
     output_dir="http_response_output_files/png"
