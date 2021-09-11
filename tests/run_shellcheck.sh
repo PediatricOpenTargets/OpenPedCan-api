5d5ccc79bb44095ef721b89b3a75c9d8a9dcb4f2
logstar <y.will.zhang@gmail.com>
2021-09-11 15:41:43 -0400

    Add tests/run_shellcheck.sh

---
+++ tests/run_shellcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+#
+# Adapted from https://stackoverflow.com/a/3355423/4638182
+cd "$(dirname "$0")" || exit
+
+cd ..
+
+# Run shellcheck on .sh files
+#
+# https://github.com/koalaman/shellcheck
+shellcheck ./db/*/*.sh ./db/*.sh ./tests/*.sh
