c6470c76e56b66f8e2cfa4431e138facfd63d806
logstar <y.will.zhang@gmail.com>
2021-09-15 13:35:55 -0400

    Increment API version

--- src/plumber.R
+++ src/plumber.R
@@ -23,7 +23,7 @@
 #*   <a href="https://github.com/PediatricOpenTargets">Pediatric Open Targets project</a>.
 
 
-#* @apiVersion v0.2.0-alpha
+#* @apiVersion v0.3.0-beta
 
 #* @apiContact list(name = "API Support", url =
 #*   "https://github.com/PediatricOpenTargets/OpenPedCan-api/issues")
