8c098f48f421b6c3f449ddc20cdfe4a211aa31ac
logstar <y.will.zhang@gmail.com>
2021-11-05 20:40:19 +0000

    Add tests/r_test_scripts/test_endpoint_http.R

---
+++ tests/r_test_scripts/test_endpoint_http.R
@@ -0,0 +1,5 @@
+# The working directory is the directory that contains this test R file, if this
+# file is executed by test_dir
+#
+# testthat package is loaded, if this file is executed by test_dir
+context("tests/r_test_scripts/test_endpoint_http.R")
