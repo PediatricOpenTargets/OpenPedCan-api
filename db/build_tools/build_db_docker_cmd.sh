c0855eb1691969d63a8a60a154f7ee0bae3d94f4
logstar <y.will.zhang@gmail.com>
2021-12-20 18:11:25 +0000

    Disable shellcheck SC2129

--- db/build_tools/build_db_docker_cmd.sh
+++ db/build_tools/build_db_docker_cmd.sh
@@ -93,6 +93,8 @@ pg_dump --clean --if-exists --no-owner --no-privileges \
 # Append SQL line(s) to create index(es).
 #
 # "Multiple  compressed  files can be concatenated." -- gzip manual page
+#
+# shellcheck disable=SC2129
 echo "CREATE INDEX tpm_ensg_id_idx ON ${BULK_EXP_SCHEMA}.${BULK_EXP_TPM_HISTOLOGY_TBL} (\"Gene_Ensembl_ID\");" \
   | gzip --no-name -c >> "$db_dump_out_path"
 
