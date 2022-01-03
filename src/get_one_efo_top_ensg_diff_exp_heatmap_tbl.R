8e4d574f55b1f9d399079fc0126027bf8c72c8f1
logstar <y.will.zhang@gmail.com>
2022-01-03 21:11:04 +0000

    Add src/get_one_efo_top_ensg_diff_exp_heatmap_tbl.R

--- src/get_one_efo_top_ensg_diff_exp_heatmap_tbl.R
+++ src/get_one_efo_top_ensg_diff_exp_heatmap_tbl.R
@@ -1,20 +1,20 @@
-# get_one_efo_top_ensg_diff_exp_heatmap.R defines a function
-# get_one_efo_top_ensg_diff_exp_heatmap to return a differential expression
-# heatmap of one EFO ID and top differentially expressed ENSG IDs.
+# get_one_efo_top_ensg_diff_exp_heatmap_tbl.R defines a function
+# get_one_efo_top_ensg_diff_exp_heatmap_tbl to return a differential expression
+# tibble of one EFO ID and top differentially expressed ENSG IDs.
 #
 # Call sequence:
 #
 # - docker run calls Rscript --vanilla main.R
-# - ../main.R calls source("src/get_one_efo_top_ensg_diff_exp_heatmap.R")
+# - ../main.R calls source("src/get_one_efo_top_ensg_diff_exp_heatmap_tbl.R")
 #
 # Defined variables:
 #
-# - get_one_efo_top_ensg_diff_exp_heatmap
+# - get_one_efo_top_ensg_diff_exp_heatmap_tbl
 
 
 
-# Get a differential expression heatmap of one EFO ID and top differentially
-# expressed ENSG IDs
+# Get a differential expression heatmap table of one EFO ID and top
+# differentially expressed ENSG IDs
 #
 # Args:
 # - efo_id: a single character value of EFO ID. Default is NULL.
@@ -58,17 +58,51 @@
 #   - "relapse_only_group": for each (Disease, cohort) tuple, keep only relapse
 #     samples.
 #
-# Returns a
-get_one_efo_top_ensg_diff_exp_heatmap <- function(
+# Returns a differential expression heatmap tibble of one EFO ID and top
+# differentially expressed ENSG IDs.
+get_one_efo_top_ensg_diff_exp_heatmap_tbl <- function(
   efo_id, rank_genes_by, max_gene_rank, y_axis_scale, cohort = NULL,
   min_n_samples_per_group = 3,
   spec_desc_group = "primary_and_relapse_same_group") {
 
   all_cohorts_str_id <- "All Cohorts"
 
-  diff_exp_heatmap_tbl <- get_one_efo_top_ensg_diff_exp_heatmap_tbl(
-    efo_id, rank_genes_by, max_gene_rank, y_axis_scale, cohort,
-    min_n_samples_per_group, spec_desc_group)
+  stopifnot(is.character(y_axis_scale))
+  stopifnot(identical(length(y_axis_scale), 1L))
+  stopifnot(y_axis_scale %in% c("linear", "log10"))
 
-  return(diff_exp_heatmap_tbl)
+  diff_exp_tbl <- get_one_efo_top_ensg_diff_exp_tbl(
+    efo_id, rank_genes_by, max_gene_rank)
+
+  if (!is.null(cohort)) {
+    stopifnot(is.character(cohort))
+    stopifnot(length(cohort) > 0)
+    stopifnot(all(!is.na(cohort)))
+
+    diff_exp_tbl <- dplyr::filter(diff_exp_tbl, .data$cohort == .env$cohort)
+  } else {
+    if (all_cohorts_str_id %in% diff_exp_tbl$cohort) {
+      diff_exp_tbl <- dplyr::filter(
+        diff_exp_tbl, .data$cohort == .env$all_cohorts_str_id)
+    } else {
+      stopifnot(identical(length(unique(diff_exp_tbl$cohort)), 1L))
+    }
+  }
+
+  stopifnot(nrow(diff_exp_tbl) > 0)
+
+  diff_exp_tbl <- dplyr::filter(
+    diff_exp_tbl, Disease_sample_count >= min_n_samples_per_group,
+    GTEx_tissue_subgroup_sample_count >= min_n_samples_per_group)
+
+  stopifnot(nrow(diff_exp_tbl) > 0)
+
+  diff_exp_log2_fc_mat <- tidyr::pivot_wider(
+    diff_exp_tbl,
+    id_cols = c(cohort, EFO, MONDO, Disease, Disease_specimen_descriptor,
+                Disease_sample_count, Gene_Ensembl_ID, Gene_symbol),
+    names_from = GTEx_tissue_subgroup,
+    values_from = log2_fold_change)
+
+  return(diff_exp_log2_fc_mat)
 }
