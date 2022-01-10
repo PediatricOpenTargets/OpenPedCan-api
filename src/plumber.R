9b69d071b48354aab10677ebe152d54b4a70ef4d
logstar <y.will.zhang@gmail.com>
2022-01-10 17:22:26 +0000

    Bump version number

--- src/plumber.R
+++ src/plumber.R
@@ -23,7 +23,7 @@
 #*   <a href="https://github.com/PediatricOpenTargets">Pediatric Open Targets project</a>.
 
 
-#* @apiVersion v0.4.0-beta
+#* @apiVersion v0.5.0-beta
 
 #* @apiContact list(name = "API Support", url =
 #*   "https://github.com/PediatricOpenTargets/OpenPedCan-api/issues")
